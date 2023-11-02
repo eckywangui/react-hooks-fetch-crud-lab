ced256fed06729127fb20185668a05be82e877ca
Eckra Wangui <+>
2023-11-02 16:16:12 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,30 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [questions, setQuestions] = useState([]); 
+  
+  const deleteQuestion = (questionId) => {
+    
+    setQuestions((prevQuestions) => prevQuestions.filter((question) => question.id !== questionId));
+  };
+
+  
+  useEffect(() => {
+    if (page === "List") {
+      fetch("http://localhost:4000/questions") 
+        .then((response) => response.json())
+        .then((data) => setQuestions(data))
+    }
+  }, [page]);
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? <QuestionForm /> : <QuestionList questions={questions} onDeleteQuestion={deleteQuestion}/>}
     </main>
   );
 }
--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, onDelete, }) {
   const { id, prompt, answers, correctIndex } = question;
+  const [newCorrectIndex, setNewCorrectIndex] = useState(correctIndex);
+
+  const handleDelete = () => {
+    onDelete(id);
+  };
+
+  const handleCorrectAnswerChange = async (event) => {
+    const newCorrectIndex = parseInt(event.target.value, 10);
+    setNewCorrectIndex(newCorrectIndex);
+
+    
+      const response = await fetch(`http://localhost:4000/questions/${id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ correctIndex: newCorrectIndex }),
+      });
+
+     
+  };
 
   const options = answers.map((answer, index) => (
     <option key={index} value={index}>
@@ -15,9 +36,11 @@ function QuestionItem({ question }) {
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select value={newCorrectIndex} onChange={handleCorrectAnswerChange}>
+          {options}
+        </select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDelete}>Delete Question</button>
     </li>
   );
 }
